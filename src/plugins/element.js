92f070b4e6d9797ffc7e938afe2173ea6dbbc7a5
‘miao0428 <462608218@qq.com>
2021-07-15 16:47:49 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,7 +37,9 @@ import {
     Steps,
     CheckboxGroup,
     Checkbox,
-    Upload
+    Upload,
+    Timeline,
+    TimelineItem
 } from 'element-ui'
 
 
@@ -77,6 +79,8 @@ Vue.use(Steps)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 
 
